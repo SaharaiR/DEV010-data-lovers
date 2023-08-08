5bd70c71d3ba82b58cfb92882bd07fc46f19c63b
Sahara <sahararod@gmail.com>
2023-08-07 19:20:26 -0600

    se presentan imagenes, numero de pokemon y nombre

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { example } from './data.js';
+//  import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
+//console.log(example, data);
 
-
-console.log(example, data);
+/*Primero cargar información general del dataset */
+const contenedor = document.querySelector('.contenedorFlex');
+for(let i = 0; i<data.pokemon.length; i++){
+  //console.log(data.pokemon[i].name);
+  contenedor.innerHTML += `<figure><img src = ${data.pokemon[i].img}><figcaption>${data.pokemon[i].name}</figcaption></figure>`
+}
