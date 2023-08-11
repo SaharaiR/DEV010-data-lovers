d78f31cea321b9990a691c8bd767ab6d892838fd
Sahara <sahararod@gmail.com>
2023-08-11 11:32:29 -0600

    borre codigo comentado

--- src/main.js
+++ src/main.js
@@ -1,34 +1,4 @@
-//  import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
-//console.log(example, data);
-
-//const longitudArray = data.pokemon.length;
-/*const longitudKanto = 151;
-const longitudJohto = 252;
-
-//Cargar los pokemons de Kanto
-//Creamos el elemento "img" de HTML, le asignamos su url obtenida del data y lo agregamos al index.html
-for(let i = 0; i<longitudKanto; i++){
-  const img = document.createElement("img");
-  img.src = data.pokemon[i].img;  
-  document.getElementById("tarjetas").appendChild(img);
-}
-
-//Cargar los pokemons de Johto
-for(let j = 152; j<longitudJohto; j++){
-  //Creamos el elemento "img" de HTML, le asignamos su url obtenida del data y lo agregamos al index.html
-  const img = document.createElement("img");
-  img.src = data.pokemon[j].img;
-  document.getElementById("tarjetas").appendChild(img);
-}
-
-/*for(let i = 0; i<longitudArray; i++){
-  //console.log(data.pokemon[i].name);
-  const numeroPkn = "No. " + data.pokemon[i].num;
-  const nombrePkn = (data.pokemon[i].name).toUpperCase();
-  const pesoAltPkn = "Altura: " + (data.pokemon[i].size.height) + " " + "Peso: " + (data.pokemon[i].size.weight);
-  tarjeta.innerHTML += `<picture><img src = ${data.pokemon[i].img}><figcaption>${numeroPkn}<br>${nombrePkn}</br><br>${pesoAltPkn}</br></figcaption></picture>`
-}*/
 
 //Cargar los botones con la referencia de las tarjetas//
 
@@ -52,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
       img.src = data.pokemon[i].img;
       tarjetas.appendChild(img);
     }
-  // Mostrar el botón "return"
+// Mostrar el botón "return"
     document.getElementById("return").style.display = "block";
     
     // Mostrar las tarjetas y ocultar el inicio
@@ -84,18 +54,3 @@ document.addEventListener("DOMContentLoaded", function () {
   // Agregar evento de clic para el botón de Kanto
   botonKanto.addEventListener("click", cargarPokemonesKanto);
 });
-
-
-
-  
-
-  
-  
-
-
-
-
-
-
-
-
