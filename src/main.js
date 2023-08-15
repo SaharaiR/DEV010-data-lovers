7f0706c3dd146f56989803e7480c235abca23475
Ketzalikab <blancoka878@gmail.com>
2023-08-15 10:59:08 -0600

    agregar botones de ascendente y de descendente

--- src/main.js
+++ src/main.js
@@ -13,9 +13,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const pokemonsKanto = 151;//donde termina
   const pokemonsJohto = 151;
   const tarjetaPorPagina = 25; //definir cuantas tarjetas se presentaran por pagina el grid  
+  const btnAsc = document.getElementById("btnAscendente");
+  const btnDes = document.getElementById("btnDescendente");
+
 
   let numeroPagina = 1; //llevar el conteo de páginas*
-  let region = ""; 
+  let region = "";
+  let botonPresionado = ""; 
+  let inicioFiltros = 0;
+  let finFiltros = 0;
 
   botonKanto.addEventListener("click", function() {
     cargarPokemonesKanto(numeroPagina); 
@@ -33,43 +39,45 @@ document.addEventListener("DOMContentLoaded", function () {
     inicio.style.display = "none";
     // Limpiamos el contenido existente en las tarjetas
     tarjetas.innerHTML = "";
-    //escuchamos el evento para realizar el cambio de paginas en las funciones correspondientes
-    
+    //limitamos el inicio y el fin para esta region
     const inicioIndice = (pagina - 1) * tarjetaPorPagina;
     const fin = Math.min(inicioIndice + tarjetaPorPagina, pokemonsKanto);
-
-    for(let i = inicioIndice; i < fin; i++) {
-      //console.log(data.pokemon[i].type);
-      const numPkn = document.createTextNode("No. " + data.pokemon[i].num);
-      const nombrePkn = document.createTextNode((data.pokemon[i].name).toUpperCase());
-      const altPkn = document.createTextNode("Height: " + (data.pokemon[i].size.height));
-      const pesoPkn = document.createTextNode("Weight: " + (data.pokemon[i].size.weight));
-      const picture = document.createElement('picture');
-      picture.classList = 'pokemon-card';
-      const img = document.createElement('img');
-      img.src = data.pokemon[i].img;
-      picture.appendChild(img);
-      const figCaption = document.createElement('figcaption');
-      figCaption.classList = 'textoFrente';
-      figCaption.appendChild(numPkn);
-      figCaption.appendChild(document.createElement('br'));
-      figCaption.appendChild(nombrePkn);
-      figCaption.appendChild(document.createElement('br'));
-      figCaption.appendChild(altPkn);
-      figCaption.appendChild(document.createElement('br'));
-      figCaption.appendChild(pesoPkn);
-      picture.appendChild(figCaption);
-      tarjetas.appendChild(picture);
-    }  
+    tarjetasFiltro(inicioIndice,fin);
     // Mostrar el botón "return"
-    btnCambio.style.display = "block";
+    document.getElementById("cambioRegion").style.display = "block";  
     // Mostrar las tarjetas y ocultar el inicio
-    //document.getElementById("marquesina").style.display = "none";
     document.getElementById("barraFiltros").style.display = "block";
     btnAnterior.style.display = "inline-block";
     btnSiguiente.style.display = "inline-block";
   }
-  //cargarPokemonesKanto(numeroPagina);
+
+  function cargarPokemonesJohto(pagina) {
+    inicio.style.display = "none";
+    marquesina.innerHTML = "Johto Region";
+    region = "johto";
+    // Limpiamos el contenido existente en las tarjetas
+    tarjetas.innerHTML = "";
+    //limitamos para esta region
+    const inicioIndice = pokemonsJohto + (pagina - 1) * tarjetaPorPagina;
+    const fin = inicioIndice + tarjetaPorPagina;
+    tarjetasFiltro(inicioIndice, fin);
+    // Mostrar el botón "return"
+    document.getElementById("cambioRegion").style.display = "block";  
+    // Mostrar las tarjetas y ocultar el inicio
+    document.getElementById("barraFiltros").style.display = "block";
+    btnAnterior.style.display = "inline-block";
+    btnSiguiente.style.display = "inline-block";
+  }
+  
+  btnCambio.addEventListener("click", function(){
+    numeroPagina = 1;
+    if(region === "kanto"){
+      cargarPokemonesJohto(numeroPagina);
+    }else if(region === "johto"){
+      cargarPokemonesKanto(numeroPagina);
+    }
+  })
+
   function pagSiguiente() {
     //sirve para limitar el numero de paginas para Kango son 151 pokemons y se necesitan 6 paginas
     const numPokemonsKanto = 151; 
@@ -81,12 +89,14 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       numeroPagina++;
       cargarPokemonesKanto(numeroPagina);
+      aplicarFiltros(numeroPagina);
     }else if(region === "johto"){
       if(numeroPagina === Math.ceil(numPokemonsJohto / tarjetaPorPagina)){
         return;
       }
       numeroPagina++;
       cargarPokemonesJohto(numeroPagina);
+      aplicarFiltros(numeroPagina);
     }
   }
   //funcion para pagina anterior
@@ -99,20 +109,23 @@ document.addEventListener("DOMContentLoaded", function () {
     //muestraPagina(numeroPagina);
     if(region === "kanto"){
       cargarPokemonesKanto(numeroPagina);
+      aplicarFiltros(numeroPagina);
     }else if(region === "johto"){
       cargarPokemonesJohto(numeroPagina);
+      aplicarFiltros(numeroPagina);
     }
   }
 
-  function cargarPokemonesJohto(pagina) {
-    inicio.style.display = "none";
-    marquesina.innerHTML = "Johto Region";
-    region = "johto";
-    // Limpiamos el contenido existente en las tarjetas
-    tarjetas.innerHTML = "";
-    //escuchamos el evento para realizar el cambio de paginas en las funciones correspondientes  
-    const inicioIndice = pokemonsJohto + (pagina - 1) * tarjetaPorPagina;
-    const fin = inicioIndice + tarjetaPorPagina;
+  btnAsc.addEventListener("click", function() {
+    numeroPagina = 1;
+    botonPresionado = "ascendente";
+    aplicarFiltros(numeroPagina); 
+  });
+  btnDes.addEventListener("click", function() {
+    numeroPagina = 1;
+    botonPresionado = "descendente";
+    aplicarFiltros(numeroPagina); 
+  });
 
   function aplicarFiltros(pagina){
     switch(botonPresionado){
