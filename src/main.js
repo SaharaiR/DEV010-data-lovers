8271a0d9e82e5a36a44a6d07e4ca6fb07a23e351
Ketzalikab <blancoka878@gmail.com>
2023-08-15 10:36:53 -0600

    arreglos de corchetes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import data from './data/pokemon/pokemon.js';
 
-
 document.addEventListener("DOMContentLoaded", function () {
   const botonKanto = document.getElementById("mostrar-tarjetas-kanto");
   const botonJohto = document.getElementById("mostrar-tarjetas-johto");
@@ -13,15 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const pokemonsKanto = 151;//donde termina
   const pokemonsJohto = 151;
   const tarjetaPorPagina = 25; //definir cuantas tarjetas se presentaran por pagina el grid  
-  const btnAsc = document.getElementById("btnAscendente");
-  const btnDes = document.getElementById("btnDescendente");
-
 
   let numeroPagina = 1; //llevar el conteo de páginas*
-  let region = "";
-  let botonPresionado = ""; 
-  let inicioFiltros = 0;
-  let finFiltros = 0;
+  let region = ""; 
 
   botonKanto.addEventListener("click", function() {
     cargarPokemonesKanto(numeroPagina); 
@@ -39,45 +32,43 @@ document.addEventListener("DOMContentLoaded", function () {
     inicio.style.display = "none";
     // Limpiamos el contenido existente en las tarjetas
     tarjetas.innerHTML = "";
-    //limitamos el inicio y el fin para esta region
+    //escuchamos el evento para realizar el cambio de paginas en las funciones correspondientes
+    
     const inicioIndice = (pagina - 1) * tarjetaPorPagina;
     const fin = Math.min(inicioIndice + tarjetaPorPagina, pokemonsKanto);
-    tarjetasFiltro(inicioIndice,fin);
+
+    for(let i = inicioIndice; i < fin; i++) {
+      //console.log(data.pokemon[i].type);
+      const numPkn = document.createTextNode("No. " + data.pokemon[i].num);
+      const nombrePkn = document.createTextNode((data.pokemon[i].name).toUpperCase());
+      const altPkn = document.createTextNode("Height: " + (data.pokemon[i].size.height));
+      const pesoPkn = document.createTextNode("Weight: " + (data.pokemon[i].size.weight));
+      const picture = document.createElement('picture');
+      picture.classList = 'pokemon-card';
+      const img = document.createElement('img');
+      img.src = data.pokemon[i].img;
+      picture.appendChild(img);
+      const figCaption = document.createElement('figcaption');
+      figCaption.classList = 'textoFrente';
+      figCaption.appendChild(numPkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(nombrePkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(altPkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(pesoPkn);
+      picture.appendChild(figCaption);
+      tarjetas.appendChild(picture);
+    }  
     // Mostrar el botón "return"
-    document.getElementById("cambioRegion").style.display = "block";  
+    btnCambio.style.display = "block";
     // Mostrar las tarjetas y ocultar el inicio
+    //document.getElementById("marquesina").style.display = "none";
     document.getElementById("barraFiltros").style.display = "block";
     btnAnterior.style.display = "inline-block";
     btnSiguiente.style.display = "inline-block";
   }
-
-  function cargarPokemonesJohto(pagina) {
-    inicio.style.display = "none";
-    marquesina.innerHTML = "Johto Region";
-    region = "johto";
-    // Limpiamos el contenido existente en las tarjetas
-    tarjetas.innerHTML = "";
-    //limitamos para esta region
-    const inicioIndice = pokemonsJohto + (pagina - 1) * tarjetaPorPagina;
-    const fin = inicioIndice + tarjetaPorPagina;
-    tarjetasFiltro(inicioIndice, fin);
-    // Mostrar el botón "return"
-    document.getElementById("cambioRegion").style.display = "block";  
-    // Mostrar las tarjetas y ocultar el inicio
-    document.getElementById("barraFiltros").style.display = "block";
-    btnAnterior.style.display = "inline-block";
-    btnSiguiente.style.display = "inline-block";
-  }
-  
-  btnCambio.addEventListener("click", function(){
-    numeroPagina = 1;
-    if(region === "kanto"){
-      cargarPokemonesJohto(numeroPagina);
-    }else if(region === "johto"){
-      cargarPokemonesKanto(numeroPagina);
-    }
-  })
-
+  //cargarPokemonesKanto(numeroPagina);
   function pagSiguiente() {
     //sirve para limitar el numero de paginas para Kango son 151 pokemons y se necesitan 6 paginas
     const numPokemonsKanto = 151; 
@@ -89,14 +80,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       numeroPagina++;
       cargarPokemonesKanto(numeroPagina);
-      aplicarFiltros(numeroPagina);
     }else if(region === "johto"){
       if(numeroPagina === Math.ceil(numPokemonsJohto / tarjetaPorPagina)){
         return;
       }
       numeroPagina++;
       cargarPokemonesJohto(numeroPagina);
-      aplicarFiltros(numeroPagina);
     }
   }
   //funcion para pagina anterior
@@ -109,48 +98,59 @@ document.addEventListener("DOMContentLoaded", function () {
     //muestraPagina(numeroPagina);
     if(region === "kanto"){
       cargarPokemonesKanto(numeroPagina);
-      aplicarFiltros(numeroPagina);
     }else if(region === "johto"){
       cargarPokemonesJohto(numeroPagina);
-      aplicarFiltros(numeroPagina);
     }
   }
 
-  btnAsc.addEventListener("click", function() {
-    numeroPagina = 1;
-    botonPresionado = "ascendente";
-    aplicarFiltros(numeroPagina); 
-  });
-  btnDes.addEventListener("click", function() {
-    numeroPagina = 1;
-    botonPresionado = "descendente";
-    aplicarFiltros(numeroPagina); 
-  });
+  function cargarPokemonesJohto(pagina) {
+    inicio.style.display = "none";
+    marquesina.innerHTML = "Johto Region";
+    region = "johto";
+    // Limpiamos el contenido existente en las tarjetas
+    tarjetas.innerHTML = "";
+    //escuchamos el evento para realizar el cambio de paginas en las funciones correspondientes  
+    const inicioIndice = pokemonsJohto + (pagina - 1) * tarjetaPorPagina;
+    const fin = inicioIndice + tarjetaPorPagina;
 
-  function aplicarFiltros(pagina){
-    switch(botonPresionado){
-    case "ascendente":
-      //presentarlos en forma ascendente en base al numero pokemon
-      if(region === "kanto"){
-        cargarPokemonesKanto(pagina);
-      }else if(region === "johto"){
-        cargarPokemonesJohto(pagina);
-      }
-      break;
-    case "descendente":
-      tarjetas.innerHTML = "";
-      if(region === "kanto"){
-        inicioFiltros = (pagina - 1) * tarjetaPorPagina;
-        finFiltros = Math.min(inicioFiltros + tarjetaPorPagina, 151);
-        tarjetasFiltro(inicioFiltros,finFiltros);
-      }else if(region === "johto"){
-        inicioFiltros = pokemonsJohto + (pagina - 1) * tarjetaPorPagina;
-        finFiltros = inicioFiltros + tarjetaPorPagina;
-        tarjetasFiltro(inicioFiltros, finFiltros);
-      }
-      break;
-    }
+    for(let i = inicioIndice; i < fin; i++) {
+      //console.log(data.pokemon[i].type);
+      const numPkn = document.createTextNode("No. " + data.pokemon[i].num);
+      const nombrePkn = document.createTextNode((data.pokemon[i].name).toUpperCase());
+      const altPkn = document.createTextNode("Height: " + (data.pokemon[i].size.height));
+      const pesoPkn = document.createTextNode("Weight: " + (data.pokemon[i].size.weight));
+      const picture = document.createElement('picture');
+      picture.classList = 'pokemon-card';
+      const img = document.createElement('img');
+      img.src = data.pokemon[i].img;
+      picture.appendChild(img);
+      const figCaption = document.createElement('figcaption');
+      figCaption.classList = 'textoFrente';
+      figCaption.appendChild(numPkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(nombrePkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(altPkn);
+      figCaption.appendChild(document.createElement('br'));
+      figCaption.appendChild(pesoPkn);
+      picture.appendChild(figCaption);
+      tarjetas.appendChild(picture);
+    }  
+    // Mostrar el botón "return"
+    document.getElementById("cambioRegion").style.display = "block";  
+    // Mostrar las tarjetas y ocultar el inicio
+    //document.getElementById("marquesina").style.display = "none";
+    document.getElementById("barraFiltros").style.display = "block";
+    btnAnterior.style.display = "inline-block";
+    btnSiguiente.style.display = "inline-block";
   }
+  btnCambio.addEventListener("click", function(){
+    numeroPagina = 1;
+    if(region === "kanto"){
+      cargarPokemonesJohto(numeroPagina);
+    }else if(region === "johto"){
+      cargarPokemonesKanto(numeroPagina);
+    }
   })
 });
 
@@ -194,57 +194,4 @@ filtrarButton.addEventListener("click", () => {
   console.log("Tipo seleccionado:", selectedType);
   console.log("Generation:", filteredByGeneration);
   console.log("Rarity:",filterByRarity)
-});
-
-  function tarjetasFiltro(inicioF, finF){
-    if(botonPresionado === "descendente"){
-      for(let i = finF - 1; i >= inicioF; i--) {
-        //console.log(data.pokemon[i].type);
-        const numPkn = document.createTextNode("No. " + data.pokemon[i].num);
-        const nombrePkn = document.createTextNode((data.pokemon[i].name).toUpperCase());
-        const altPkn = document.createTextNode("Height: " + (data.pokemon[i].size.height));
-        const pesoPkn = document.createTextNode("Weight: " + (data.pokemon[i].size.weight));
-        const picture = document.createElement('picture');
-        picture.classList = 'pokemon-card';
-        const img = document.createElement('img');
-        img.src = data.pokemon[i].img;
-        picture.appendChild(img);
-        const figCaption = document.createElement('figcaption');
-        figCaption.classList = 'textoFrente';
-        figCaption.appendChild(numPkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(nombrePkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(altPkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(pesoPkn);
-        picture.appendChild(figCaption);
-        tarjetas.appendChild(picture);
-      }
-    }else{
-      for(let i = inicioF; i < finF; i++) {
-        //console.log(data.pokemon[i].type);
-        const numPkn = document.createTextNode("No. " + data.pokemon[i].num);
-        const nombrePkn = document.createTextNode((data.pokemon[i].name).toUpperCase());
-        const altPkn = document.createTextNode("Height: " + (data.pokemon[i].size.height));
-        const pesoPkn = document.createTextNode("Weight: " + (data.pokemon[i].size.weight));
-        const picture = document.createElement('picture');
-        picture.classList = 'pokemon-card';
-        const img = document.createElement('img');
-        img.src = data.pokemon[i].img;
-        picture.appendChild(img);
-        const figCaption = document.createElement('figcaption');
-        figCaption.classList = 'textoFrente';
-        figCaption.appendChild(numPkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(nombrePkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(altPkn);
-        figCaption.appendChild(document.createElement('br'));
-        figCaption.appendChild(pesoPkn);
-        picture.appendChild(figCaption);
-        tarjetas.appendChild(picture);
-      }
-    }
-  }
 });
