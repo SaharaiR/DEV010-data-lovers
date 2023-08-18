272aaf97199c6acf8c85a7c7f25d364a7c003cdf
Ketzalikab <blancoka878@gmail.com>
2023-08-18 00:55:20 -0600

    se agrega funcion para girar tarjetas, estilo de barra de filtros, el import para la data.spect

--- src/data.js
+++ src/data.js
@@ -1,7 +1,6 @@
 // estas funciones son de ejemplo
 const dataFunction = {
   filterGeneration: (data, generation) => { 
-
     const filteredPokemons = data.filter(pokemon => {
       return pokemon.generation.num.toLowerCase() === generation.toLowerCase();
     });
@@ -18,25 +17,63 @@ const dataFunction = {
   filterByRarity: (pokemons, selectedRarity) => {
     return pokemons.filter(pokemon => pokemon['pokemon-rarity'] === selectedRarity);
   },
+  sortDescendent: (pokemons) => {
+    return pokemons.sort((a, b) => {
+      if (a.num > b.num) {
+        return -1;
+      }
+      if (a.num < b.num) {
+        return 1;
+      }
+      return 0;
+    });
+  },
+  searching: (pokemon, nameOrNumber) => {
+    return pokemon.find(p => {
+      return p.num === nameOrNumber || p.name === nameOrNumber; 
+    });
+  },
+    
+  /*applyFilters: (data, selectedGeneration, selectedType, selectedRarity) => {
+    let filteredPokemons = data;
   
-  combineFilteredPokemon: (data, generation, selectType, selectRarity) => {
-    let filteredPokemons = data; 
+    if (selectedGeneration !== 'cero') {
+      filteredPokemons = filteredPokemons.filter(pokemon => {
+        return pokemon.generation.num.toLowerCase() === selectedGeneration.toLowerCase();
+      });
+    }
+  
+    if (selectedType !== 'cero') {
+      filteredPokemons = filteredPokemons.filter(pokemon => {
+        return pokemon.type.includes(selectedType);
+      });
+    }
+  
+    if (selectedRarity !== 'cero') {
+      filteredPokemons = filteredPokemons.filter(pokemon => {
+        return pokemon['pokemon-rarity'] === selectedRarity;
+      });
+    }
+  
+    return filteredPokemons;*/
+}
+  
+/*filterCombine: (pokemons, rarity, type, generation) => {
+    //let filtered = pokemons;
+    if(rarity) {
+      //filtered = filtered.filter(p => p['pokemon-rarity'] === rarity);
+      pokemons = pokemons.filter(p => p['pokemon-rarity'] === rarity);
+    }
+    if(type) {
+      pokemons = pokemons.filter(p => p['pokemon-type'] === type);
+      //filtered = filtered.filter(p => p['pokemon-type'] === type); 
+    }
+    if(generation) {
+      pokemons = pokemons.filter(p => p['pokemon-generation'] === generation);
+      //filtered = filtered.filter(p => p['pokemon-generation'] === generation);
+    }
+    return pokemons;
+  }*/
 
-    if (generation) {
-      filteredPokemons = dataFunction.filterGeneration(filteredPokemons, generation);
-    }
-    if (selectType){
-      filteredPokemons = dataFunction.filterByType(filteredPokemons, selectType);
-    }
-    if (selectRarity){
-      filteredPokemons = dataFunction.filterByRarity(filteredPokemons, selectRarity);
-    }
 
-    return filteredPokemons;
-  }
-}; 
 export default dataFunction;
-
-
-// const anotherExample = () => {
-// return 'OMG';
